5f6488bd8a347ea256ae8cb51d524f2b9ee9df1f
Stephen Eisenhauer <stephen@hellosign.com>
2016-01-30 14:28:25 -0800

    Fix build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
 var webpack = require('webpack');
 module.exports = {
-    entry: "./js/entry.jsx",
-    output: {
-        path: __dirname + "/build",
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel', // 'babel-loader' is also a legal name to reference
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            },
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    },
-    plugins: [
-        new webpack.NoErrorsPlugin()
-    ]
+  entry: "./js/entry.jsx",
+  devtool: "#source-map",
+  output: {
+    path: __dirname + "/build",
+    filename: "bundle.js",
+    sourceMapFilename: "[file].map"
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel', // 'babel-loader' is also a legal name to reference
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }, {
+      test: /\.css$/,
+      loader: "style!css"
+    }]
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ]
 };
